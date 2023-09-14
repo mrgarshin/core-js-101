587f9a333d9e5a0aa6fd977684a0f2e6025a2109
mrgarshin <garshin.serg@yandex.ru>
2023-09-14 16:18:53 +0300

    feat: add all solutions fot number tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -162,7 +162,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
   */
 function getParallelepipedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2) + c ** 2;
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -204,8 +204,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0 || n < 2) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -223,8 +228,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 module.exports = {
