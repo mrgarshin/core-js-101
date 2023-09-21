629ab6092708086498ae0c71854cddcf991aad2f
mrgarshin <garshin.serg@yandex.ru>
2023-09-21 13:56:43 +0300

    feat: add getUpperCaseString task solving

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -126,8 +126,8 @@ function removeFalsyValues(arr) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getUpperCaseStrings(arr) {
+  return arr.map((i) => i.toUpperCase());
 }
 
 
